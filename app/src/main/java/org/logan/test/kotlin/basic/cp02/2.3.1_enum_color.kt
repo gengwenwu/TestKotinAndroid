00a1818fa6193f46e7c7bd235b8de7e7c819016c
Logan <490211581@qq.com>
2019-08-22 17:31:56 +0800

    Kotlin 枚举

---
+++ app/src/main/java/org/logan/test/kotlin/basic/cp02/2.3.1_enum_color.kt
@@ -0,0 +1,31 @@
+package org.logan.test.kotlin.basic.cp02
+
+
+/**
+ * desc: Kotlin枚举 <br/>
+ * time: 2019-08-22 17:26 <br/>
+ * author: Logan <br/>
+ * since V 1.0 <br/>
+ */
+
+// Kotlin用了enum class声明一个枚举，而Java只使用enum一个关键字
+enum class Color {
+    RED, ORANGE, YELLOW, GREEN, BLUE, INDIGO, VIOLET
+}
+
+// 给枚举声明属性和方法
+enum class Color2(
+    val r: Int, val g: Int, val b: Int // 属性
+) {
+    RED(255, 0, 0),
+    ORANGE(255, 165, 0),
+    YELLOW(255, 255, 0),
+    GREEN(0, 255, 0),
+    BLUE(0, 0, 255),
+    INDIGO(75, 0, 130),
+    VIOLET(238, 130, 238); //Kotlin唯一分号的地方
+
+    // 方法
+    fun rgb() = (r * 256 + g) * 256 + b
+
+}
