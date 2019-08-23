5fe9e09d25fa111a640cf34285e3b43dc7e0ec30
Logan <490211581@qq.com>
2019-08-23 14:46:27 +0800

    优化智能转换

--- app/src/main/java/org/logan/test/kotlin/basic/cp02/2.3.5_SmartCasts.kt
+++ app/src/main/java/org/logan/test/kotlin/basic/cp02/2.3.5_SmartCasts.kt
@@ -14,9 +14,10 @@ interface Expr
 class Num(val value: Int) : Expr // 声明类，并实现Expr接口
 class Sum(val left: Expr, val right: Expr) : Expr
 
+
 fun eval(e: Expr): Int {
     if (e is Num) {
-        val n = e as Num // 显示转换成Num，是多余的操作
+        val n = e as Num // 使用as关键字显示转换成Num，是多余的操作
         return e.value // 智能转换后，绿色背景高亮提示
     }
 
@@ -26,3 +27,41 @@ fun eval(e: Expr): Int {
 
     throw IllegalArgumentException("Unknown expression")
 }
+
+
+/**
+ * 简化 eval()函数 - if表达式 <br/>
+ * 1，省略花括号。如果 if分支中只有一个表达式，最外层花括号省略掉。<br/>
+ * 2，去掉return。如果 if分支是一个代码块，代码块中的最后一个表达式被作为结果返回。<br/>
+ **/
+fun eval2(e: Expr): Int =
+    if (e is Num) {
+        e.value
+    } else if (e is Sum) {
+        eval(e.left) + eval(e.right)
+    } else {
+        throw IllegalArgumentException("Unknown expression")
+    }
+
+
+/**
+ * if表达式代码块 <br/>
+ * if 和 when 都可以使用 代码块 作为分支体。这种情况下，代码块中的最后一个表达式就是结果。<br/>
+ * 规则：“代码块中最后的表达式就是结果”，在所有使用代码块并期望得到一个结果的表达式都成立。<br/>
+ * 这个规则对常规函数不成立。一个函数要么是表达式体函数，要么具有包含显示return语句的代码块函数体。<br/>
+ **/
+fun evalWithLog(e: Expr): Int =
+    when (e) {
+        //
+        is Num -> { // 码块作为分支体
+            println("num: ${e.value}")
+            e.value
+        }
+        //
+        is Sum -> {
+            println("sum: ${eval(e.left) + eval(e.right)}")
+            eval(e.left) + eval(e.right)
+        }
+        //
+        else -> throw IllegalArgumentException("Unknown expression")
+    }
