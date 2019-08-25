81a80499d319893922060ef2a026239017cb28fa
Logan <490211581@qq.com>
2019-08-25 21:09:15 +0800

    Kotlin 命名函数参数

--- app/src/main/java/org/logan/test/kotlin/basic/cp03/3.2.1_JoinToString.kt
+++ app/src/main/java/org/logan/test/kotlin/basic/cp03/3.2.1_JoinToString.kt
@@ -1,10 +1,7 @@
 package org.logan.test.kotlin.basic.cp03
 
 /**
- * desc: Kotlin 中创建集合 <br/>
- * Kotlin没有采用自己的集合类，而是采用Java标准库中的集合类。<br/>
- * 因为使用标准Java集合类，Kotlin可以更容易与Java代码交互。<br/>
- * 尽管Kotlin的集合类和Java的集合类完全一致，但Kotlin为集合提供了更多的api。<br/>
+ * desc: 以命名参数名称方式调用Kotlin函数 <br/>
  * time: 2019-08-25 20:23 <br/>
  * author: Logan <br/>
  * since V 1.0 <br/>
@@ -19,7 +16,7 @@ fun main(args: Array<String>) {
 
     // Kotlin命名参数方式调用
     // 如果在调用一个函数时，指明了一个参数名称，那它之后的所有参数都需要标明名称。
-    // 当你在Java中调用该函数时，依旧不能采用命名参数方式
+    // 当你在Java中调用该函数时(joinToString)，依旧不能采用命名参数方式
     println(joinToString(list, prefix = "(", postfix = ")", separator = ";"))
 
 
