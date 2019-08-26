4f6b58325f233e168d12a5b5bea85c8093802608
Logan <490211581@qq.com>
2019-08-26 17:07:58 +0800

    最终版 FinalJoinToString

---
+++ app/src/main/java/org/logan/test/kotlin/basic/cp03/3.3.3_FinalJoinToString.kt
@@ -0,0 +1,32 @@
+package org.logan.test.kotlin.basic.cp03
+
+/**
+ * desc: 最终版Collection.joinToString  <br/>
+ * time: 2019-08-26 16:53 <br/>
+ * author: Logan <br/>
+ * since V 1.0 <br/>
+ */
+fun <T> Collection<T>.joinToString( // 为Collection
+    separator: String = ", ",
+    prefix: String = " ",
+    postfix: String = ""
+): String {
+    val result = StringBuilder(prefix)
+
+    for ((index, element) in this.withIndex()) {
+        if (index > 0) result.append(separator)
+
+        result.append(element)
+    }
+
+    result.append(postfix)
+    return result.toString()
+}
+
+
+fun main() {
+    val list = arrayListOf(1, 2, 3)
+    println(list.joinToString("|"));
+
+}
+
