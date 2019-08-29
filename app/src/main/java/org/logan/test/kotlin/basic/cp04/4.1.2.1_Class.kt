cbd8625c41352cd015d56cc896a7d201208c6236
Logan <490211581@qq.com>
2019-08-29 13:32:35 +0800

    Kotlin 类

---
+++ app/src/main/java/org/logan/test/kotlin/basic/cp04/4.1.2.1_Class.kt
@@ -0,0 +1,29 @@
+package org.logan.test.kotlin.basic.cp04
+
+/**
+ * desc: Kotlin 类 <br/>
+ * 1，Class类默认是 public final 的。类默认是final带来了一个重要的好处就是这使得在大量场景中的智能转换成为可能。<br/>
+ * 2，类中的函数默认是public final；添加open后转换成可重写 (非final)。 <br/>
+ * 3，实现、重写 父类函数，Kotlin强制使用override关键字。并自动变成open的。 <br/>
+ * -
+ * open、final和abstract修饰符：默认为final
+ * time: 2019-08-29 11:24 <br/>
+ * author: Logan <br/>
+ * since V 1.0 <br/>
+ */
+open class RichButton : Clickable {
+
+    // 属性默认 private
+    var text1: String? = null;
+    val text2: String? = null;
+
+    // 类函数默认 public final，不能在子类中重写
+    fun disable() {}
+
+    // open表示可以在子类中重写它
+    open fun animate() {}
+
+    // 重写了一个open的函数，它本身同样就是open的。添加final关键字，变成非open的。
+    final override fun click() {}
+
+}
