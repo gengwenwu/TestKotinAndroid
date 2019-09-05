208451acfbfbdbf83e21043a0fedbbab0266d959
Logan <490211581@qq.com>
2019-09-05 11:49:46 +0800

    Lambda - 代码块作为参数传递

---
+++ app/src/main/java/org/logan/test/kotlin/basic/cp05/5.1.1_CodeBlockAsMethodArgs.kt
@@ -0,0 +1,35 @@
+package org.logan.test.kotlin.basic.cp05
+
+import android.widget.Button
+
+/**
+ * desc: 函数式编程 - Lambada。主要作用：
+ * 1，作为函数参数的代码块 <br/>
+ * 2，和集合一起工作。<br/>
+ *
+ * 函数式编程提供另外一种解决方案：把函数当作值来对待，可以直接传递函数，而不需要先声明一个类再传递这个类的实例。<br/>
+ *
+ * time: 2019-09-05 11:25 <br/>
+ * author: Logan <br/>
+ * since V 1.0 <br/>
+ */
+
+fun testSimpleLambda() {
+    val button: Button? = null
+
+    // 1，Java 事件监听器
+    // button.setOnClickListener(new OnClickListener () {
+    //     @Override
+    //     public void onClick(View view) {
+    //     }
+    // });
+
+    // 2，Java lambda形式 事件监听器
+    // button.setOnClickListener(view -> {});
+
+
+    // 3, Kotlin用lambda实现监听器
+    button?.setOnClickListener {}
+
+    // 上面3种效果是一模一样的
+}
