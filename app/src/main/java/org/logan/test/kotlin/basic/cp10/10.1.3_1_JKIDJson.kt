69fbb6952a866f49ba5e8926f779d2f7b6b0c574
Logan <490211581@qq.com>
2019-09-09 16:56:54 +0800

    Kotlin jkid json库

---
+++ app/src/main/java/org/logan/test/kotlin/basic/cp10/10.1.3_1_JKIDJson.kt
@@ -0,0 +1,22 @@
+package org.logan.test.kotlin.basic.cp10
+
+import ru.yole.jkid.deserialization.deserialize
+import ru.yole.jkid.serialization.serialize
+
+/**
+ * desc: 使用jkid库 处理json <br/>
+ * time: 2019-09-09 16:38 <br/>
+ * author: Logan <br/>
+ * since V 1.0 <br/>
+ */
+
+data class Person(val name: String, val age: Int)
+
+fun main() {
+
+    val person = Person("Alice", 29)
+    println(serialize(person)) // 使用serialize将对象转换为json字符串
+
+    val json = """ {"name": "Alice", "age": 29} """
+    println(deserialize<Person>(json)) // 使用deserialize将json字符串转换为对象
+}
---
+++ app/src/main/java/org/logan/test/kotlin/basic/cp10/10.1.3_2_JKIDCustomJson.kt
@@ -0,0 +1,28 @@
+package org.logan.test.kotlin.basic.cp10
+
+import ru.yole.jkid.JsonExclude
+import ru.yole.jkid.JsonName
+import ru.yole.jkid.deserialization.deserialize
+import ru.yole.jkid.serialization.serialize
+
+/**
+ * desc: jkid库 使用注解自定义解析过程 <br/>
+ * time: 2019-09-09 16:45 <br/>
+ * author: Logan <br/>
+ * since V 1。0 <br/>
+ */
+data class Person2(
+    // 使用 @JsonName 指定新名称
+    @JsonName("alias") val firstName: String,
+    // 使用 @JsonExclude 排除字段
+    @JsonExclude val age: Int? = null // 注意，必须指定age的默认值。否则，在反序列化时你无法创建一个Person的新实例。
+)
+
+fun main() {
+    val json = """ {"alias": "Alice", "age": 29} """
+    val person = deserialize<Person2>(json)
+
+    println(person) // 输出：Person2(firstName=Alice, age=29)
+    println(serialize(person)) // 输出：{"alias": "Alice"}
+
+}
