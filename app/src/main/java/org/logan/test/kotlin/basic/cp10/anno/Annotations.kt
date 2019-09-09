b9835e9d4f9b83cd082e9cdc3cdf12a2051b47e4
Logan <490211581@qq.com>
2019-09-09 16:25:35 +0800

    Kotlin注解

--- app/src/main/java/org/logan/test/kotlin/basic/cp10/anno/Annotations.kt
+++ app/src/main/java/org/logan/test/kotlin/basic/cp10/anno/Annotations.kt
@@ -3,12 +3,17 @@ package org.logan.test.kotlin.basic.cp10.anno
 import kotlin.reflect.KClass
 
 /**
- * desc: Kotlin 注解声明的地方 <br/>
+ * desc: Kotlin 注解声明  <br/>
  * time: 2019-09-09 14:33 <br/>
  * author: Logan <br/>
  * since V 1.0 <br/>
  */
 
-annotation class Anno(val args1: KClass<*>, val arg2: KClass<out Any>)
+annotation class Anno( // Kotlin使用annotation关键字声明注解
+    val args1: KClass<*>,
+    val arg2: KClass<out Any>
+)
+
+// Kotlin注解不能有任何实现
 
 
