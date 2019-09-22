0678c5cd56b8d7ae612b79f0b45d91cd0a9554f9
Logan <490211581@qq.com>
2019-09-22 18:31:17 +0800

    Kotlin安全调用运算符?.

---
+++ app/src/main/java/org/logan/test/kotlin/basic/cp06/6.1.3_SafeCallOperator.kt
@@ -0,0 +1,58 @@
+package org.logan.test.kotlin.basic.cp06
+
+import android.annotation.SuppressLint
+
+/**
+ * desc: Kotlin安全调用运算符: ?. <br/>
+ * 它允许你把一次 null 检查和一次方法调用合并成一个操作。例如：<br/>
+ * 表达式 s?.toUpperCase() 等同于这种写法: if (s != null) s.toUpperCase() else null <br/>
+ *
+ * time: 2019-09-22 18:13 <br/>
+ * author: Logan <br/>
+ * since V 1.0 <br/>
+ */
+
+// 例1：使用安全操作符
+@SuppressLint("DefaultLocale")
+fun printAllCaps(s: String?) {
+    // s如果非空，toUpperCase()调用会被正常地执行。如果s为null，整个表达式返回null
+    val allCaps: String? = s?.toUpperCase() // allCaps可能为null
+    println(allCaps)
+}
+
+
+// 例2：安全调用可空属性
+class Employee(val name: String, val manager: Employee?) // manager属性可以为null
+
+fun managerName(employee: Employee): String? = employee.manager?.name // 安全调用manager name属性
+
+
+// 例3：链接多个安全调用
+class Address(val streetAddress: String, val zipCode: Int, val city: String, val country: String)
+
+class Company(val name: String, val address: Address?)
+
+class Person(val name: String, val company: Company?)
+
+fun Person.countryName(): String {
+    val country = this.company?.address?.country // 多个安全符一起调用
+    return if (country != null) country else "Unknown"
+}
+
+
+fun main() {
+    printAllCaps("abc")
+    printAllCaps(null)
+
+
+    println()
+    val ceo = Employee("Alice", null)
+    val developer = Employee("Bob Smith", ceo)
+    println(managerName(ceo))
+    println(managerName(developer))
+
+
+    println()
+    val person = Person("Dmitry", null)
+    println(person.countryName())
+}
