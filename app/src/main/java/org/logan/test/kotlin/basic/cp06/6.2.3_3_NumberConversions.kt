d4809796895f9fd6f82cfa6f4633a7e0206d9a00
Logan <490211581@qq.com>
2019-11-27 14:19:49 +0800

    Kotlin数字转换

---
+++ app/src/main/java/org/logan/test/kotlin/basic/cp06/6.2.3_3_NumberConversions.kt
@@ -0,0 +1,14 @@
+package org.logan.test.kotlin.basic.cp06
+
+/**
+ * desc: 数字转换 - 字符串转换 <br/>
+ * Kotlin 标准库提供了一套相似的扩展方法，用来把字符串转换成基本数据类型( toint、 toByte、 toBoolean 等)
+
+ * time: 2019-11-27 14:18 <br/>
+ * author: Logan <br/>
+ * since V 1.0 <br/>
+ */
+fun main() {
+    // 每个这样的函数都会尝试把字符串的内容解析成对应的类型，如果解析失败则抛出 NumberFormatException。
+    println("42".toLong())
+}
