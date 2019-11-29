069933f7de235b703712071a3e79fe5794ecb6ab
Logan <490211581@qq.com>
2019-11-29 11:22:24 +0800

    只读集合与可变集合

---
+++ app/src/main/java/org/logan/test/kotlin/basic/cp06/6.3.2_ReadonlyAndMutableCollections.kt
@@ -0,0 +1,35 @@
+package org.logan.test.kotlin.basic.cp06
+
+/**
+ * desc: 只读集合与可变集合 <br/>
+ * Kotlin的集合和Java不同，它把访问集合数据的接口和修改集合数据的接口分开了。
+ * 只读：kotlin.collections.Collection
+ * 读写：kotlin.collections.MutableCollection，其中MutableCollection是继承上面的Collection的。
+ *
+ * 一般的规则：优先使用只读接口Collection，只在代码需要修改集合的地方使用MutableCollection。
+ *
+ * 使用集合接口时需要牢记的一个关键点：只读集合不一定是不可变的。
+ *
+ * 只读集合并不总是线程安全的。如果你在多线程环境下处理数据，
+ * 需要保证代码正确地同步了对数据的访问，或者使用支持并发访问的数据结构。
+ *
+ * time: 2019-11-28 15:39 <br/>
+ * author: Logan <br/>
+ * since V 1.0 <br/>
+ */
+
+// 使用只读集合接口与可变集合接口
+fun <T> copyElement(source: Collection<T>, target: MutableCollection<T>) {
+    for (item in source) {
+        target.add(item)
+    }
+}
+
+
+fun main() {
+    val source: Collection<Int> = arrayListOf(3, 5, 7)
+    val target: Collection<Int> = arrayListOf(1)  // MutableCollection
+    copyElement(source, target) // target 参数错误，不能把只读集合类型的变量作为target参数传给函数copyElement()
+    println(target)
+
+}
