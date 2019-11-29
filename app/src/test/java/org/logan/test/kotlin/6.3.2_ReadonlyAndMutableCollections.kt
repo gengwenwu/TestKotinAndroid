a3ed65853ac37c9122235863bc73245e9e81331f
Logan <490211581@qq.com>
2019-11-29 14:53:44 +0800

    集合测试

--- app/src/test/java/org/logan/test/kotlin/6.3.2_ReadonlyAndMutableCollections.kt
+++ app/src/test/java/org/logan/test/kotlin/6.3.2_ReadonlyAndMutableCollections.kt
@@ -1,4 +1,6 @@
-package org.logan.test.kotlin.basic.cp06
+package org.logan.test.kotlin
+
+import java.util.concurrent.TimeUnit
 
 /**
  * desc: 只读集合与可变集合 <br/>
@@ -10,7 +12,7 @@ package org.logan.test.kotlin.basic.cp06
  *
  * 使用集合接口时需要牢记的一个关键点：只读集合不一定是不可变的。
  *
- * 只读集合并不总是线程安全的。如果你在多线程环境下处理数据，
+ * 读写集合并不总是线程安全的。如果你在多线程环境下处理数据，
  * 需要保证代码正确地同步了对数据的访问，或者使用支持并发访问的数据结构。
  *
  * time: 2019-11-28 15:39 <br/>
@@ -29,7 +31,27 @@ fun <T> copyElement(source: Collection<T>, target: MutableCollection<T>) {
 fun main() {
     val source: Collection<Int> = arrayListOf(3, 5, 7)
     val target: Collection<Int> = arrayListOf(1)  // MutableCollection
-    copyElement(source, target) // target 参数错误，不能把只读集合类型的变量作为target参数传给函数copyElement()
-    println(target)
+    // copyElement(source, target) // target 参数错误，不能把只读集合类型的变量作为target参数传给函数copyElement()
+    // println(target)
+
+
+    // 测试现成安全问题
+    val list1: MutableCollection<Int> = arrayListOf();
+    val list2: MutableCollection<Int> = list1
+
+    // 加数据
+    Thread(Runnable {
+        for (i in 0 until 1000_000) {
+            list1.add(i)
+        }
+    }).start()
+
+    // 写数据
+    Thread(Runnable {
+        TimeUnit.MILLISECONDS.sleep(2)
+        for (i in list2) {
+            println(i)
+        }
+    }).start()
 
 }
